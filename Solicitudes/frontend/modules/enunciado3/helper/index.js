33f0ec6957ae6c13b4c63fa7d0a881cd93f172ee
Sergio Andrés <sergio19855@gmail.com>
2026-04-20 16:31:27 -0500

    Se configuro la estrurura de carpetas por enunciados

---
+++ Solicitudes/frontend/modules/enunciado3/helper/index.js
@@ -0,0 +1,5 @@
+export * from "./delete.js";
+export * from "./get.js";
+export * from "./patch.js";
+export * from "./posts.js";
+export * from "./put.js";
---
+++ Solicitudes/frontend/modules/enunciado4/posts/index.js
@@ -0,0 +1,5 @@
+export * from "./createPosts.js";
+export * from "./deletePosts.js";
+export * from "./getPosts.js";
+export * from "./patchPosts.js";
+export * from "./updatePosts.js";
