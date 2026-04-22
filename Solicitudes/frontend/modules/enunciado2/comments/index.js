c47d5b660df550e0c55b4d95ef73af793d67a564
Sergio Andres <sergio19855@gmail.com>
2026-04-22 11:09:26 -0500

    Se resolvieron conflictos

--- Solicitudes/frontend/modules/enunciado2/comments/index.js
+++ Solicitudes/frontend/modules/enunciado2/comments/index.js
@@ -1,7 +1,6 @@
 export * from "./createComments.js";
-// export * from "./deleteComments.js";
 export * from "./getComments.js";
 export * from "./updateComments.js";
 
 
-export { getComments } from "./getComments.js"; 
+// export { getComments } from "./getComments.js";
